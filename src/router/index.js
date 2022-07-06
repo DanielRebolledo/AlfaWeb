d49964dc87ef70c293b82a193d236e388f70b34f
Daniel Rebolledo <43189313+DanielRebolledo@users.noreply.github.com>
2022-07-06 19:42:58 -0400

    primer commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,50 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import UsersView from "../views/UsersView";
+import CoursesView from "../views/CoursesView.vue"
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    path: "/UsersView",
+    name: "UsersView",
+    component: UsersView,
+  },
+  {
+    path: "/AdminView",
+    name: "AdminView",
+
+    component: () => import("../views/AdminView.vue"),
+  },
+  {
+    path: "/CoursesView",
+    name: "CoursesView",
+    component: CoursesView,
+
+    accessLevel: "public",
+    // meta: {
+    //   accessLevel: "private",
+    // },
+  },
+  {
+    path: "/EditView",
+    name: "EditView",
+
+    component: () => import("../views/EditView.vue"),
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
